0c52623feb760d728ff73cea5b49c4844f905c9a
Oleg <oleg94038@gmail.com>
2024-04-17 19:15:07 +0200

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['simple-import-sort', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
+  printWidth: 120,
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 2,
+  importOrder: ['<THIRD_PARTY_MODULES>', '^@app/(.*)$', '^\\./(.*)$', '^\\.\\./(.*)$'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
